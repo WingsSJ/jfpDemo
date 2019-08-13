21fe747aa1619711db133c736764a561ddac7a51
Wings <Wings_s@126.com>
2019-08-13 10:20:14 +0800

    develop

---
+++ gRPCService/src/main/java/com/demo/grpc/interceptor/PtokenServerInterceptor.java
@@ -0,0 +1,92 @@
+package com.demo.grpc.interceptor;
+
+import com.demo.grpc.server.Constants;
+import com.topsec.ptoken.core.TokenUtils;
+import com.topsec.ptoken.core.ValidResult;
+import com.topsec.ptoken.ext.mobile.TokenDataMobile;
+import io.grpc.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.UUID;
+
+@Component
+@Scope("prototype")
+@Slf4j
+public class PtokenServerInterceptor extends BaseServerInterceptor {
+    @Autowired
+    private TokenUtils tokenUtils;
+    @Value("${myauthentication.ptoken.ptokenKeys}")
+    private String[] ptokenKeys;
+
+    @Override
+    protected <ReqT, RespT> ServerCall.Listener<ReqT> toChildInterceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        String ptoken = headers.get(Constants.PTOKEN_META_KEY);
+        boolean isAuth=false;
+        ValidResult validResult = null;
+        if(ptokenKeys!=null && ptokenKeys.length>0){
+            for (String key : ptokenKeys) {
+                validResult = tokenUtils.authToken(ptoken, key);
+                if(validResult.isValid()){
+                    isAuth=validResult.isValid();
+                    break;
+                }
+            }
+        }
+
+        if (isAuth){
+            if(log.isDebugEnabled()){
+                log.debug("mobile auth success");
+            }
+            String _mobiid = headers.get(Constants.SUBDIG_META_KEY);
+            TokenDataMobile tokenDataMobile = validResult.parseData(TokenDataMobile.class);
+            String tokenSnSha1Base64=null;
+            try {
+                MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+                String sn = tokenDataMobile.getSn();
+                byte[] digest = sha1.digest(sn.getBytes());
+                tokenSnSha1Base64 = Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+
+            } catch (NoSuchAlgorithmException e) {
+                String uuid=UUID.randomUUID().toString();
+                log.error("["+uuid+"] "+e.getMessage(),e);
+                call.close(Status.UNKNOWN.withDescription(uuid),headers);
+                return NOOP_LISTENER;
+            }
+            if(_mobiid == null||_mobiid.length()<=0){
+                if(log.isDebugEnabled()){
+                    log.debug("header subdig is null _mobileId:{},tokenSnSha1Base64:{}",_mobiid,tokenSnSha1Base64);
+                }
+                call.close(Status.UNAUTHENTICATED.withDescription("400100#缺少有效认证凭证"),headers);
+                return NOOP_LISTENER;
+            }
+
+            if(!_mobiid.equals(tokenSnSha1Base64)){
+                if(log.isDebugEnabled()){
+                    log.debug("mobile device sn error _mobileId:{},tokenSnSha1Base64:{}",_mobiid,tokenSnSha1Base64);
+                }
+                call.close(Status.UNAUTHENTICATED.withDescription("400101#认证错误"),headers);
+                return NOOP_LISTENER;
+            }
+            Context ctx = Context.current().withValue(Constants.PTOKEN_CTX_KEY, validResult.getToken());
+
+            return Contexts.interceptCall(ctx, call, headers, next);
+
+        } else{
+            if(log.isDebugEnabled()){
+                log.debug("mobile auth error info {}",validResult.getErrorCode());
+            }
+            call.close(Status.UNAUTHENTICATED
+                            .withDescription("400301#认证错误")
+                    , headers
+            );
+            return NOOP_LISTENER;
+        }
+    }
+}
---
+++ gRPCService/src/main/java/com/demo/grpc/interceptor/UnknownStatusDescriptionInterceptor.java
@@ -0,0 +1,92 @@
+package com.demo.grpc.interceptor;
+
+import com.demo.grpc.server.Constants;
+import com.topsec.util.ObjectUtil;
+import io.grpc.*;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 统一未知异常处理
+ *
+ * @auther jasonx
+ * @create 5/22/18 11:57 AM
+ **/
+@Slf4j
+public class UnknownStatusDescriptionInterceptor implements ServerInterceptor {
+
+    private static final ServerCall.Listener NOOP_LISTENER = new ServerCall.Listener() {
+    };
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        //验证手机唯一标示是否合法
+//        String mobiid = headers.get(Constants.MOBIID_META_KEY);
+//        if(mobiid!=null && mobiid.length()>0){
+//            Context context = Context.current().withValue(Constants.MOBIID_CTX_KEY, mobiid);
+//
+//        }
+//        if(mobiid==null || mobiid.length()<=0){
+//            //手机唯一标示不能为空
+//            if(log.isDebugEnabled()){
+//                log.warn("mobile auth error. _mobileId:{}",mobiid);
+//            }
+//            call.close(Status.INVALID_ARGUMENT
+//                            .withDescription("400100#缺少有效认证凭证")
+//                    , headers
+//            );
+//            return NOOP_LISTENER;
+//        }else if(mobiid.length()>100){
+//            //手机唯一标示长度最大100
+//            if(log.isDebugEnabled()){
+//                log.warn("mobile data too long. _mobileId:{}",mobiid);
+//            }
+//            call.close(Status.INVALID_ARGUMENT
+//                            .withDescription("400101#缺少有效认证凭证")
+//                    , headers
+//            );
+//            return NOOP_LISTENER;
+//        }
+        //验证手机软件版本
+        String mobiSoftVer = headers.get(Constants.MOBI_SOFT_VER);
+        //必须是数字类型的字符串
+        if(ObjectUtil.isNumber(mobiSoftVer) && ObjectUtil.parseInteger(mobiSoftVer)>=1){
+            Integer softVer = ObjectUtil.parseInteger(mobiSoftVer);
+//            int diffver = Constants.SOFTVER - softVer;
+            if(!softVer.equals(1)){
+                if(log.isDebugEnabled()){
+                    log.debug("mobile auth error,version too old. version:{}",softVer);
+                }
+                call.close(Status.INVALID_ARGUMENT
+                                .withDescription("400200#请升级软件")
+                        , headers
+                );
+                return NOOP_LISTENER;
+            }
+        }else{
+            if(log.isDebugEnabled()){
+                log.debug("mobile auth error. version:{}",mobiSoftVer);
+            }
+            call.close(Status.INVALID_ARGUMENT
+                            .withDescription("400201#认证错误")
+                    , headers
+            );
+            return NOOP_LISTENER;
+        }
+
+        ServerCall<ReqT, RespT> wrappedCall = new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call){
+            @Override
+            public void close(Status status, Metadata trailers) {
+                if (status.getCode() == Status.Code.UNKNOWN && status.getDescription()!=null) {
+                    Throwable e = status.getCause();
+                    String description = "500000# [" + status.getDescription() + "] 服务器内部错误! ";
+                    status = Status.UNKNOWN
+                            .augmentDescription(description);
+                    log.error(description,e);
+                }
+                super.close(status, trailers);
+            }
+
+        };
+        return next.startCall(call,headers);
+//        return Contexts.interceptCall(context, wrappedCall, headers, next);
+    }
+}
